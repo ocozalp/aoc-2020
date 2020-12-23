192db00817041efb7175cef69f968fa9bba46485
ocozalp <ozalp.orhan@gmail.com>
2020-12-23 12:02:08 -0500

    Q10

---
+++ q10.py
@@ -0,0 +1,42 @@
+from collections import defaultdict
+
+
+def solve():
+  with open('input/in.txt', 'r') as f:
+    lines = [int(line[:-1]) for line in f.readlines()]
+
+  print('Answer 1:', solve1(lines[:]))
+  print('Answer 2:', solve2(lines[:]))
+
+
+def solve1(lines):
+  lines.sort()
+
+  current = 0
+  diffs = [0] * 4
+  for i, num in enumerate(lines):
+    diff = num - current
+    if diff > 3:
+      return None
+    diffs[diff] += 1
+    current = num
+  return diffs[1] * (diffs[3]+1)
+
+
+def solve2(lines):
+  lines.sort()
+  last = max(lines) + 3
+  ways_to_go = defaultdict(int)
+  lines.append(last)
+  lines.insert(0, 0)
+
+  ways_to_go[0] = 1
+  for curr in lines:
+    w = ways_to_go[curr]
+    for j in range(1, 4):
+      ways_to_go[curr + j] += w
+
+  return ways_to_go[last]
+
+
+solve()
