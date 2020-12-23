ba34c0b5cf83bd0218c0f011c08a022fbd73b63d
ocozalp <ozalp.orhan@gmail.com>
2020-12-23 11:52:10 -0500

    Q6

---
+++ q6.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+
+
+def solve():
+  with open('input/in.txt', 'r') as f:
+    lines = list(map(lambda s: s[:-1], f.readlines()))
+
+  print('Answer 1:', solve1(lines[:]))
+  print('Answer 2:', solve2(lines[:]))
+
+def solve1(lines):
+  current = set()
+  res = 0
+  for line in lines:
+    if line == '':
+      res += len(current)
+      current = set()
+    else:
+      current |= set(line)
+  res += len(current)
+  return res
+
+
+def solve2(lines):
+  current = defaultdict(int)
+  currcount = 0
+  res = 0
+  for line in lines:
+    if line == '':
+      if currcount != 0:
+        for a in current:
+          if current[a] == currcount:
+            res += 1
+        current = defaultdict(int)
+        currcount = 0
+    else:
+      for c in line:
+        current[c] += 1
+      currcount += 1
+  for a in current:
+    if current[a] == currcount:
+      res += 1
+  return res
+
+solve()
