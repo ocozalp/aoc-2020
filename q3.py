5432bc19f3c08efec2e85c371a0cc476dd097746
ocozalp <ozalp.orhan@gmail.com>
2020-12-23 11:34:55 -0500

    Q3

---
+++ q3.py
@@ -0,0 +1,28 @@
+def solve():
+  with open('input/in.txt', 'r') as f:
+    lines = list(map(lambda s: s[:-1], f.readlines()))
+
+  print('Answer 1:', solve1(lines[:]))
+  print('Answer 2:', solve2(lines[:]))
+
+
+def count_trees(lines, c, r):
+  col = 0
+  cols = len(lines[0])
+  count = 0
+  for row in range(r, len(lines), r):
+    col = (col + c) % cols
+    if lines[row][col] == '#':
+      count += 1
+  return count
+
+
+def solve1(lines):
+  return count_trees(lines, 3, 1)
+
+
+def solve2(lines):
+  calc = lambda c, r: count_trees(lines, c, r)
+  return calc(1, 1) * calc(3, 1) * calc(5, 1) * calc(7, 1) * calc(1, 2)
+
+solve()
